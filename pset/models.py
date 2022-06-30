7d8007dce0aa5314f1d4aa4084010bf57ad0aadf
AmanKas <aman312000@gmail.com>
2022-07-01 00:39:43 +0530

    Database Table Design

--- pset/models.py
+++ pset/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from datetime import datetime  
+from django.contrib.auth.models import User
 
 # Create your models here.
--- register/models.py
+++ register/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime  
 
 # Create your models here.
+
+
+# Users Profiles
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    total_score = models.FloatField(default = 0)
+
+#Problemset
+
+class Problems(models.Model):
+    pcode = models.AutoField(primary_key = True)
+    pdesc=models.TextField()
+    difficulty = models.CharField(max_length = 100)
+    solved_status = models.CharField(max_length = 50)
+    score = models.FloatField(default = 0)
+
+    def __str__(self) :
+        return self.pcode
+
+class TestCases(models.Model):
+    pcode = models. OneToOneField(Problems, verbose_name = ('problem_id'), on_delete = models.CASCADE, primary_key = True)
+    inp=models.FileField(upload_to='testcases',blank=True)
+    out=models.FileField(upload_to='testcases',blank=True)
+
+    def __str__(self):
+        return self.pcode
+
+class Submissions(models.Model):
+    submission_code = models.AutoField(primary_key = True)
+    pcode = models.ForeignKey(Problems, verbose_name = ('problem_id'), on_delete = models.CASCADE)
+    user_id = models.ForeignKey(UserProfile, verbose_name = ('user.id'), on_delete = models.CASCADE)
+    timestamp = models.DateTimeField(default = datetime.now, blank = True)
+
+    def __str__(self):
+        return str(self.timestamp)
